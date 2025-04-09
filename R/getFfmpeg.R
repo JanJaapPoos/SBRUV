464ea38a0e3dc8e66069588f8fb2cb1f16e5daf9
JanJaapPoos <janjaap.poos@wur.nl>
2025-04-09 16:57:34 +0200

    Update getFfmpeg.R suppressed all warnings

--- R/getFfmpeg.R
+++ R/getFfmpeg.R
@@ -1,21 +1,23 @@
 getFfmpeg <-  function(){
-  statusFfmpeg <-  system("ffmpeg -v", show.output.on.console = F, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE)
-  if (statusFfmpeg == 127){
+  suppressWarnings({
+    statusFfmpeg <-  system("ffmpeg -v", show.output.on.console = F, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE)
+    if (statusFfmpeg == 127){
 
-    oldTimeOut <- getOption("timeout")
-    options(timeout = 300)
-    download.file("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", destfile = "ffmpeg.zip")
-    allfiles <- list.files()
-    unzip("ffmpeg.zip")
-    newfile <- setdiff(list.files(), allfiles)
+      oldTimeOut <- getOption("timeout")
+      options(timeout = 300)
+      download.file("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", destfile = "ffmpeg.zip")
+      allfiles <- list.files()
+      unzip("ffmpeg.zip")
+      newfile <- setdiff(list.files(), allfiles)
   
-    file.copy(file.path(newfile,"/bin/ffmpeg.exe"), "ffmpeg.exe")
-    file.remove("ffmpeg.zip")
-    file.remove("./ffmpeg-7.1.1-essentials_build", recursive = T, showWarnings = FALSE)
+      file.copy(file.path(newfile,"/bin/ffmpeg.exe"), "ffmpeg.exe")
+      file.remove("ffmpeg.zip")
+      file.remove("./ffmpeg-7.1.1-essentials_build", recursive = T, showWarnings = FALSE)
 
-    options(timeout = oldTimeOut)
+      options(timeout = oldTimeOut)
     
-  } else {
-    print("ffmpeg available, no need to download and install")
-  }
-}
+    } else {
+      print("ffmpeg available, no need to download and install")
+    }
+  })
+} 
