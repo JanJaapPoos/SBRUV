5650ec76a37a2d081e6f6cd5021a62dd624ade79
JanJaapPoos <janjaap.poos@wur.nl>
2025-04-09 16:51:21 +0200

    Update getFfmpeg.R fixinf timeout for download file

--- R/getFfmpeg.R
+++ R/getFfmpeg.R
@@ -1,7 +1,10 @@
 getFfmpeg <-  function(){
   statusFfmpeg <-  system("ffmpeg -v", show.output.on.console = F, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE)
   if (statusFfmpeg == 127){
-    download.file("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", destfile = "ffmpeg.zip", timeout = 180)
+
+    oldTimeOut <- getOption("timeout")
+    options(timeout = 300)
+    download.file("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", destfile = "ffmpeg.zip")
     allfiles <- list.files()
     unzip("ffmpeg.zip")
     newfile <- setdiff(list.files(), allfiles)
@@ -9,6 +12,9 @@ getFfmpeg <-  function(){
     file.copy(file.path(newfile,"/bin/ffmpeg.exe"), "ffmpeg.exe")
     file.remove("ffmpeg.zip")
     file.remove("./ffmpeg-7.1.1-essentials_build", recursive = T, showWarnings = FALSE)
+
+    options(timeout = oldTimeOut)
+    
   } else {
     print("ffmpeg available, no need to download and install")
   }
