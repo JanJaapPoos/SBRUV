4e378e7001441b1641edc870ff52d7dfb80fe181
JanJaapPoos <janjaap.poos@wur.nl>
2025-04-14 15:36:16 +0200

    Update extractFramesDir.R fixed bug

--- R/extractFramesDir.R
+++ R/extractFramesDir.R
@@ -3,12 +3,12 @@ extractFramesDir <- function (img.dir = "obs_videos", output.dir = NULL,
 {
   if (is.null(output.dir)) 
     output.dir <- "Images"
-  dir.create(getwd(), img.dir, output.dir))
+  dir.create(getwd(), img.dir, output.dir)
   for (ii in list.files(img.dir, pattern = pattern, include.dirs = F)) {
     print(ii)
     mov.name <- strsplit(ii, "\\.")[[1]][1]
     output.dir2 <- paste0(getwd(),"/",img.dir,"/",output.dir, "/", mov.name)
-    dir.create(getwd(), img.dir, output.dir, mov.name))
+    dir.create(getwd(), img.dir, output.dir, mov.name)
     cmd1 <- paste0("ffmpeg -i ", img.dir, "\\", ii, " -vsync vfr ", 
                    output.dir2, "/%04d.jpg")
     system(cmd1, show.output.on.console = F)
