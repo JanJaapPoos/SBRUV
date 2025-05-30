072102cae5a45f8b2a6a69532701d168e23ef66f
JanJaapPoos <janjaap.poos@wur.nl>
2025-05-30 16:12:09 +0200

    Update extractFramesDir.R
    Added nth argument that allows taking every nth frame when extracting frames

--- R/extractFramesDir.R
+++ R/extractFramesDir.R
@@ -1,16 +1,20 @@
-extractFramesDir <- function (img.dir = "obs_videos", output.dir = NULL, 
-          pattern = "MP4") 
-{
+extractFramesDir <- function (img.dir = "obs_videos", output.dir = NULL, pattern = "MP4", nth = 1) {
   if (is.null(output.dir)) 
     output.dir <- "Images"
   dir.create(file.path(getwd(), img.dir, output.dir))
+
   for (ii in list.files(img.dir, pattern = pattern, include.dirs = F)) {
     print(ii)
     mov.name <- strsplit(ii, "\\.")[[1]][1]
-    output.dir2 <- paste0(img.dir,"/",output.dir, "/", mov.name)
+    output.dir2 <- paste0(img.dir, "/", output.dir, "/", mov.name)
     dir.create(file.path(getwd(), img.dir, output.dir, mov.name))
-    cmd1 <- paste0("ffmpeg -i ", img.dir, "\\", ii, " -vsync vfr ", 
-                   output.dir2, "/%04d.jpg")
+   
+    cmd1 <- paste0(
+      'ffmpeg -i "', img.dir, '/', ii, 
+      '" -vf "select=not(mod(n\\,', nth, '))" -vsync 0 -frame_pts 1 "', 
+      output.dir2, '/%d.jpg"'
+    )
+    
     system(cmd1, show.output.on.console = F)
   }
 }
