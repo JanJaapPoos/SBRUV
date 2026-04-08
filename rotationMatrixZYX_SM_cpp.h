35bec136f43c37ad19d3fc6a41e839df775ad3fe
JanJaapPoos <janjaap.poos@wur.nl>
2026-04-08 16:47:57 +0200

    Delete rotationMatrixZYX_SM_cpp.h

--- rotationMatrixZYX_SM_cpp.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROTATION_MATRIX_ZYX_SM_CPP_H
-#define ROTATION_MATRIX_ZYX_SM_CPP_H
-
-#include <Rcpp.h>
-using namespace Rcpp;
-
-NumericMatrix rotationMatrixZYX_SM_cpp(const NumericVector& t);
-
-#endif
