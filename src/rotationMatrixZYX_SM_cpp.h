9ec73962d7f54987182c22e1cb02e977d863f2a0
JanJaapPoos <janjaap.poos@wur.nl>
2026-04-08 16:30:42 +0200

    Move rotationMatrixZYX_SM_cpp.h to src directory

---
+++ src/rotationMatrixZYX_SM_cpp.h
@@ -0,0 +1,9 @@
+#ifndef ROTATION_MATRIX_ZYX_SM_CPP_H
+#define ROTATION_MATRIX_ZYX_SM_CPP_H
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+NumericMatrix rotationMatrixZYX_SM_cpp(const NumericVector& t);
+
+#endif
